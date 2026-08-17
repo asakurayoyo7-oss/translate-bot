a3c8a0e165beee7ed8f3aefe5cf911e9e3ff9939
asakurayoyo7-oss <asakurayoyo7@gmail.com>
2026-08-17 14:13:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ async def translate_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         response_message = "Xin lỗi, đã có lỗi xảy ra khi dịch."
 
     await update.message.reply_text(response_message)
-    if __name__ == '__main__':
+
+if __name__ == '__main__':
     application = ApplicationBuilder().token("8684404526:AAERMQiQRE5rTTaBeVuqVzdaKSFBqmCiuAc").build()
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), translate_text))
     application.run_polling()
